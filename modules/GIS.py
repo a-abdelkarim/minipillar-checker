b6418744761a75810262612c7433902a2efbeb73
a-abdelkarim <a.abdelkarim9696@gmail.com>
2022-02-13 09:19:33 +0200

    create GIS class

---
+++ modules/GIS.py
@@ -0,0 +1,17 @@
+class GIS:
+    """Handle GIS Functions"""
+    def __init__(self):
+        pass
+    
+    # Methods
+    def json_to_file(self, json_object):
+        """convert geojson object to file
+
+        Args:
+            json_object (dict): geojson feature
+        
+        Return:
+            file_path (str): path of the geojson file
+        """
+        pass
+        
